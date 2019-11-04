1cc180d1ccc56fccfd28e2a05539d141f562e0bc
gabrielarck <stanislasvega@yahoo.fr>
2019-11-04 10:38:21 +0100

    add input button

---
+++ src/components/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() =>  setCount(count*0)}>
+        Reset
+      </button>
+      <p>Le compteur est  à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
